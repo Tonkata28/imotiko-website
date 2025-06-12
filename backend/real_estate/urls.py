7bf3e4be62ab99dfc66f7699c5a1656b91b011c1
Tonkata28 <tonisimeonov33@gmail.com>
2025-06-12 14:27:04 +0300

    Update

--- backend/real_estate/urls.py
+++ backend/real_estate/urls.py
@@ -6,10 +6,12 @@ router = DefaultRouter()
 router.register(r'properties', views.PropertyViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('properties/featured/', views.FeaturedPropertiesView.as_view(), name='featured-properties'),
+    # Non-property related endpoints
     path('stats/', views.PropertyStatsView.as_view(), name='property-stats'),
     path('inquiries/', views.PropertyInquiryCreateView.as_view(), name='property-inquiries'),
     path('favorites/toggle/', views.FavoriteToggleView.as_view(), name='favorite-toggle'),
     path('favorites/', views.FavoriteListView.as_view(), name='favorite-list'),
+    
+    # Router includes all property endpoints
+    path('', include(router.urls)),
 ]
